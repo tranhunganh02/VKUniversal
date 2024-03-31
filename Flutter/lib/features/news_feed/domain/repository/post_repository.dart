4611efca214306a399cf1acf7ddbabb98579ea07
huyxoann <huytn.21it@vku.udn.vn>
2024-03-31 13:51:00 +0700

    feat:add post repository at domain

--- Flutter/lib/features/news_feed/domain/repository/post_repository.dart
+++ Flutter/lib/features/news_feed/domain/repository/post_repository.dart
@@ -1,6 +1,6 @@
 import 'package:vkuniversal/core/resourses/data_state.dart';
 import 'package:vkuniversal/features/news_feed/domain/entities/post.dart';
 
-abstract class PostRepository {
+abstract interface class PostRepository {
   Future<DataState<List<PostEntity>>> getPosts();
 }
